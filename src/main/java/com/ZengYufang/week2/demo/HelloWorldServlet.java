971f77aa1c302747d73f2af7dbaf0c8f57757d55
Luckybaby1987 <369619498@qq.com>
2021-03-15 21:01:07 +0800

    HW - CE1 - Commit

---
+++ src/main/java/com/ZengYufang/week2/demo/HelloWorldServlet.java
@@ -0,0 +1,26 @@
+package com.ZengYufang.week2.demo;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+public class HelloWorldServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        PrintWriter writer = response.getWriter();
+        writer.println("Name : ZengYufang");
+        writer.println("ID : 2019211001000803");
+        Date date = new Date();
+        writer.println("Date and Time " + date.toString());
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        PrintWriter writer = response.getWriter();
+        writer.println("Hello Client!!!");
+    }
+}
