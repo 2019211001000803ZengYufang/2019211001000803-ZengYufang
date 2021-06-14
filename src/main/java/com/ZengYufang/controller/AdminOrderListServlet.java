2829b3546ae45312b6c71c2f626564f5222db2ac
Luckybaby1987 <369619498@qq.com>
2021-06-14 23:04:53 +0800

    HE - week15 - Commit

---
+++ src/main/java/com/ZengYufang/controller/AdminOrderListServlet.java
@@ -0,0 +1,36 @@
+package com.ZengYufang.controller;
+
+import com.ZengYufang.dao.OrderDao;
+import com.ZengYufang.model.Order;
+import com.ZengYufang.model.Payment;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.List;
+
+@WebServlet(name = "AdminOrderListServlet", value = "/admin/orderList")
+public class AdminOrderListServlet extends HttpServlet {
+    private Connection con = null;
+    public void init(){
+        con = (Connection)getServletContext().getAttribute("dbConn");
+    }
+    public void destroy(){
+        super.destroy();
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Payment> paymentTypeList= Payment.findAllPayment(con);
+        request.setAttribute("paymentTypeList",paymentTypeList);
+        OrderDao orderDao = new OrderDao();
+        List<Order> orderList = orderDao.findAll(con);
+        request.setAttribute("orderList",orderList);
+        String path="/WEB-INF/views/admin/orderList.jsp";
+        request.getRequestDispatcher(path).forward(request,response);
+    }
+}
